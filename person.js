be0abe82f01ea9f6699bbcafd602636ed6ad850d
Saahil <29069313+SALTx@users.noreply.github.com>
2023-08-07 04:35:12 +0800

    Update person.js

--- person.js
+++ person.js
@@ -1,9 +1,25 @@
+/**
+ * Person class
+ * @class
+ * @param {string} name the name of the person
+ * @param {number} amountSpent the amount spent by the person
+ */
 export default class Person {
+  /**
+   * @constructor
+   * @param {string} name the name of the person
+   * @param {number} amountSpent the amount spent by the person
+   */
   constructor(name, amountSpent) {
     this.name = name;
     this.amountSpent = amountSpent;
   }
 
+  /**
+   * @param {Person} sender the person who is sending the money
+   * @param {Person} receiver the person who is receiving the money
+   * @param {number} amount the amount of money being transferred
+   */
   static transferMoney(sender, receiver, amount) {
     sender.amountSpent += amount;
     receiver.amountSpent -= amount;
